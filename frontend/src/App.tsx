ec9fe251ec5cb5cd9595bb39531a5c0e28dce7a6
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-09 17:07:00 +0600

    Использовал MUI. Использовал компоненты созданные ранее. Создал функцию перезагрузки при нажатии на тулбар

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import MessageList from './components/MessageList.tsx';
+import SendMessage from './components/SendMessage.tsx';
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import WhatsApp from '@mui/icons-material/WhatsApp';
+
+const App = () => {
+  const handleRefresh = () => {
+    window.location.reload();
+  }
+  return (
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            onClick={handleRefresh}
+            variant="h6"
+            sx={{flexGrow: 1, cursor: 'pointer'}}
+          >
+            ChatApp
+          </Typography>
+          <WhatsApp
+            onClick={handleRefresh}
+            sx={{cursor: 'pointer'}}
+          />
+        </Toolbar>
+      </AppBar>
+      <MessageList/>
+      <SendMessage/>
+    </Container>
+  );
+};
+
+export default App;
