81a7bcdec96fa8d537317fe2a68340ec46a16b4c
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-09 16:03:18 +0600

    Создал хранилище. Создал хуки.

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,11 @@
+import {configureStore} from "@reduxjs/toolkit";
+import MessagesReducer from '../features/messagesSlice.ts';
+
+export const store = configureStore({
+ reducer: {
+     messages: MessagesReducer,
+ },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
